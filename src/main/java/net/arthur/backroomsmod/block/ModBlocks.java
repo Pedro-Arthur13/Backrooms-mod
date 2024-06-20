491c3d82b766df4d21c2a252cfbee959d5e57107
Pedro Arthur <parthurmalbuquerque@gmail.com>
2024-06-20 14:07:23 -0300

    loot tables issue

--- src/main/java/net/arthur/backroomsmod/block/ModBlocks.java
+++ src/main/java/net/arthur/backroomsmod/block/ModBlocks.java
@@ -5,11 +5,13 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
@@ -18,13 +20,17 @@ public class ModBlocks {
             new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK)));
 
     public static final Block DEEPSLATE_PINK_GARNET_ORE_BLOCK = registerBlock("deepslate_pink_garnet_ore_block",
-            new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK)));
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_IRON_ORE),
+                    UniformIntProvider.create(3,6))); // quanto de xp vai droppar(entre 3,6)
     public static final Block END_PINK_GARNET_ORE_BLOCK = registerBlock("end_pink_garnet_ore_block",
-            new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK)));
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.END_STONE),
+                    UniformIntProvider.create(2,3)));
     public static final Block NETHER_PINK_GARNET_ORE_BLOCK = registerBlock("nether_pink_garnet_ore_block",
-            new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK)));
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.NETHER_GOLD_ORE),
+                    UniformIntProvider.create(3,6)));
     public static final Block PINK_GARNET_ORE_BLOCK = registerBlock("pink_garnet_ore_block",
-            new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK)));
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.IRON_ORE),
+                    UniformIntProvider.create(3,5)));
     public static final Block BACKROOM_WALL = registerBlock("backroom_wall",
             new Block(FabricBlockSettings.copyOf(Blocks.BEDROCK)));
     public static final Block BACKROOM_FLOOR = registerBlock("backroom_floor",
